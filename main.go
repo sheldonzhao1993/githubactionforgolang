311d62bd5f2c1650c5cf6d5ad29406c46aadca6c
Automated Publisher <actions@users.noreply.github.com>
2020-03-11 06:52:26 +0000

    Automated publish: Wed Mar 11 06:52:26 UTC 2020 b0cabf2a86752380fa8fbe6d8401d6d77e93d17f

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 func main() {
 	fmt.Println("hello    world")
-fmt.Println("hello no indent")
-fmt.Println("hello no indent")
-fmt.Println("hello no indent")
-fmt.Println("hello no indent")
+	fmt.Println("hello no indent")
+	fmt.Println("hello no indent")
+	fmt.Println("hello no indent")
+	fmt.Println("hello no indent")
 }
