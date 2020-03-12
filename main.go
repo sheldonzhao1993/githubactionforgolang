eba117cb4f60db1d0b59f70aabd26ed1e56cfb77
rongyi <sqrongyi@163.com>
2020-03-12 16:35:53 +0800

    Update master

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ func main() {
 	fmt.Println("hello    world")
 	fmt.Println("hello no indent")
 	fmt.Println("hello no indent")
-	fmt.Println("hello no indent")
-	fmt.Println("hello no indent")
 fmt.Println("hello no indent")
 fmt.Println("hello no indent")
 }
