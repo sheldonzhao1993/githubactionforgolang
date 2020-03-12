22dc5f933b9b94b91e8b3b9b7dc6551b02f8a1de
null (Rebase PR Action) <null@users.noreply.github.com>
2020-03-12 08:00:20 +0000

    go format code

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-fmt.Println("hello    world")
+	fmt.Println("hello    world")
 	fmt.Println("hello no indent")
 	fmt.Println("hello no indent")
 	fmt.Println("hello no indent")
